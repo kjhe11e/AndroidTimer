68a07ea4dfffbe2f170269714df124b9fb29cc28
Kyle Helle <kjhelle4@gmail.com>
2017-01-10 03:15:21 -0800

    first commit

---
+++ app/src/main/java/com/example/khe11e/timer/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.khe11e.timer;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+
+    Chronometer clock;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clock = (Chronometer) findViewById(R.id.clock);
+        clock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restart();
+            }
+        });
+
+    }
+
+    protected void restart() {
+        clock.setBase(SystemClock.elapsedRealtime());
+        clock.start();
+    }
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
